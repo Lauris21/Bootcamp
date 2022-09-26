25b5a0e24b7a8b028f711d599ee3704bb4d197cb
Lauris-21 <laurasanchez0301@gmail.com>
2022-09-26 22:29:21 +0200

    feat iteration4

--- Javascript/neoland-basic-algorithms/iteration3.js
+++ Javascript/neoland-basic-algorithms/iteration3.js
@@ -8,3 +8,11 @@ console.log(x / z);
 let a = 15;
 let b = 9;
 console.log(a % b);
+
+let p = 10;
+let j = 5;
+let o = p + j;
+
+let c = 10;
+let m = 5;
+let i = 10 * 5;
---
+++ Javascript/neoland-basic-algorithms/iteration4.js
@@ -0,0 +1,9 @@
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+avengers[0] = "IRONMAN"
+
+console.log(avengers.length);
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters);
