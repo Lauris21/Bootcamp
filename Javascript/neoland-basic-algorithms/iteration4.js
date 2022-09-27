0d9fb9e3f0a0039311def6538b0709d2bdab1a5f
Lauris-21 <laurasanchez0301@gmail.com>
2022-09-27 17:27:11 +0200

    Algorithms

--- Javascript/neoland-basic-algorithms/iteration4.js
+++ Javascript/neoland-basic-algorithms/iteration4.js
@@ -7,3 +7,11 @@ console.log(avengers.length);
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 rickAndMortyCharacters.push("Morty", "Summer");
 console.log(rickAndMortyCharacters);
+
+rickAndMortyCharacters.push("Lapiz Lopez");
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters);
+console.log(rickAndMortyCharacters[0], rickAndMortyCharacters[rickAndMortyCharacters.length-1]);
+
+rickAndMortyCharacters.splice(1, 1);
+console.log(rickAndMortyCharacters);
