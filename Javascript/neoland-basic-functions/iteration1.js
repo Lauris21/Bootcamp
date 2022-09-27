8e6e1801b81e0c9d08557fbc249633ef7cc80639
Lauris-21 <laurasanchez0301@gmail.com>
2022-09-27 17:40:02 +0200

    Iteration1

---
+++ Javascript/neoland-basic-functions/iteration1.js
@@ -0,0 +1,11 @@
+function sum (numberOne, numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log(numberOne);
+    } else {
+        console.log(numberTwo);
+    }
+}
+
+sum(50,2);
+
+sum (0.2, -0.1200);
