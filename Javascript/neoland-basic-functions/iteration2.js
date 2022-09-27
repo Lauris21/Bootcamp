63485614088871aaad2bc08a3074a2387d1d89a8
Lauris-21 <laurasanchez0301@gmail.com>
2022-09-27 19:03:30 +0200

    Iteration2

---
+++ Javascript/neoland-basic-functions/iteration2.js
@@ -0,0 +1,14 @@
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function largestWord(list) {
+    let maxLength = 0;
+    for (let i = 0; i < list[i].length; i++) {
+        if (list[i].length > maxLength) {
+            maxLength = list[i].length; 
+        }
+
+        }
+    }
+}
+
+largestWord(avengers);
---
+++ Javascript/neoland-basic-functions/iteration3.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(array) {
+    for (let i = 0; i < array.length; i++) {
+        let x = i;
+        ;
+    }
+}
+console.log(sumAll(numbers));
