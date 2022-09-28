c698d6f09279e4e1e4703446e5f3e2f9c2cf7e6e
Lauris-21 <laurasanchez0301@gmail.com>
2022-09-28 18:09:22 +0200

    feat iteration1

--- Javascript/neoland-basic-functions/iteration1.js
+++ Javascript/neoland-basic-functions/iteration1.js
@@ -1,4 +1,4 @@
-function sum (numberOne, numberTwo) {
+/*function sum (numberOne, numberTwo) {
     if (numberOne > numberTwo) {
         console.log(numberOne);
     } else if (numberOne < numberTwo){
@@ -9,4 +9,15 @@ function sum (numberOne, numberTwo) {
 sum(50,2);
 
 sum (0.2, -0.1200);
-sum(3, 3);
+sum(3, 3);*/
+
+const sum = (numberOne, numberTwo) => {
+    if (numberOne > numberTwo) {
+        return numberOne
+    } else if (numberOne < numberTwo){
+        return numberTwo
+    } else {
+        return "Son iguales";
+    }
+}
+console.log(sum(20, 40));
--- Javascript/neoland-basic-functions/iteration2.js
+++ Javascript/neoland-basic-functions/iteration2.js
@@ -2,14 +2,16 @@ function largestWord(list) {
     let minLength = 0;
     let maxLength = 0;
     let longestWord = [];
-    for (let i = 0; i < list[i].length; i++) {
+    for (let i = 0; i < list.length; i++) {
         if (list[i].length > minLength) {
             minLength = list[i].length;
-        }  else {list[i].length = maxLength}
+        }  
 
         }
         console.log(minLength, maxLength);
     }
     
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+
 largestWord(avengers);
