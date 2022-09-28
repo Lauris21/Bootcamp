f3c1513c65131435900c3e6bfc3989949e0e505f
Lauris-21 <laurasanchez0301@gmail.com>
2022-09-28 20:49:46 +0200

    Iteration3

--- Javascript/neoland-basic-functions/iteration3.js
+++ Javascript/neoland-basic-functions/iteration3.js
@@ -1,19 +1,10 @@
 const numbers = [1, 2, 3, 5, 45, 37, 58];
-
-
-/*function sumAll(array) {
-    let suma = 0;
+let suma = 0;
+const sumAll = (array) => {
     for (let i = 0; i < array.length; i++) {
         suma += array[i];
-        return suma;
     }
+    return suma;
 }
 
-console.log(sumAll(numbers));*/
-let sum = 0;
-function sumAll(numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
-        return console.log(sum);
-    }
-}
+console.log(sumAll(numbers));
