4df601fdbe60d4f7a552d74531efdb08de9a1da8
Lauris-21 <laurasanchez0301@gmail.com>
2022-09-30 20:08:05 +0200

    Iteration2

---
+++ Javascript/neoland-review-basics/iteration2.js
@@ -0,0 +1,45 @@
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+let volumens = 0;
+let average = 0;
+let contador = 0;
+let valor = 0;
+
+for (const user of users) {
+    const {favoritesSounds} = user;
+    for (let key in favoritesSounds) {
+        let valor = favoritesSounds[key].volume;
+       volumens = volumens + valor;
+        contador++;
+        average = volumens / contador;
+    }
+}
+    console.log(average);
