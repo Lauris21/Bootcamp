2424dff56e0283f37e5f0436185a186a0d9a37f6
Lauris-21 <laurasanchez0301@gmail.com>
2022-10-02 14:04:08 +0200

    Iteration1

---
+++ Javascript/neoland-dom/iteration1/app.js
@@ -0,0 +1,15 @@
+const buttonShow = document.querySelector(".showme");
+console.log(buttonShow);
+
+const queryH1 = document.querySelector("#pillado");
+console.log(queryH1);
+
+const allP = document.querySelectorAll("p");
+console.log(allP);
+
+const pokeClass = document.querySelectorAll(".pokemon");
+console.log(pokeClass);
+
+const atributoTestMe = document.querySelectorAll("[data-function=testMe]");
+console.log(atributoTestMe);
+console.log(atributoTestMe[2]);
