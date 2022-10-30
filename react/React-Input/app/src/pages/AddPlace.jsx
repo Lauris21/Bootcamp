92c33bdcf4380ffca8e4e088aabd388d8736c1a0
Lauris-21 <laurasanchez0301@gmail.com>
2022-10-30 18:59:39 +0100

    React-Form

--- react/React-Input/app/src/pages/AddPlace.jsx
+++ react/React-Input/app/src/pages/AddPlace.jsx
@@ -1,8 +1,70 @@
-
+import { useState } from "react";
 
 const AddPlace = () => {
+
+    const [newPlace, setnewPlace] = useState({})
+
+    const [name, setName] = useState("");
+    const [ubication, setUbication] = useState("");
+    const [image, setImage] = useState("");
+    const [description, setDescription] = useState("");
+
+    const createPlace = (ev) => {
+     let place = {
+            name: name,
+            ubication: ubication,
+            image: image,
+            description: description,
+            id: uuid(),
+        }
+    }
+
     return (
-        <div className="addPlace">AddPlace</div>
+        <div className="addPlace">
+            <h4>Hi 🤙🏽, in this form you can add the data to create a post with your favorite place 🌴.</h4>
+            <form onSubmit={(ev) => createPlace(ev)}>
+                <fieldset>
+                    <legend>Add the data</legend>
+                        <div>
+                            <label htmlFor="name">Name:</label>
+                            <input type="text"
+                             name="name"
+                             id="name" 
+                             onChange={(ev) => setName(ev.target.value)}/>
+                        </div>
+                        <div>
+                            <label htmlFor="name">Ubication:</label>
+                            <input type="text"
+                             name="ubication" 
+                             id="ubication" 
+                             onChange={(ev) => setUbication(ev.target.value)}/>
+                        </div>
+                        <div>
+                            <label htmlFor="image">Image:</label>
+                            <input type="text"
+                             name="image" 
+                             id="image" 
+                             onChange={(ev) => setImage(ev.target.value)}/>
+                        </div>
+                        <div>
+                            <label htmlFor="image">Description:</label>
+                            <input type="text"
+                             name="description" 
+                             id="description" 
+                             onChange={(ev) => setDescription(ev.target.value)}/>
+                        </div>
+                </fieldset>
+                <input type="submit" value="Post" />
+            </form>
+            <div className="preview">
+                <h3>Preview</h3>
+                <figure>
+                    <h4>{name}, {ubication}</h4>
+                    <img src={image} slt={name} />
+                    <p>{description}</p>
+                </figure>
+            </div>
+        </div>
     )
 }
 
--- react/React-Input/app/src/pages/Home.jsx
+++ react/React-Input/app/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 
 const Home = () => {
     return (
-        <div className="home">Home</div>
+        <div className="home">
+            <p className="introHome">Welcome to the blog where you can discover interesting places.</p>
+            <p className="addHome">You can also post your place, you just have to add the name, location, image with link and a short description.</p>
+            <figure className="gifHome">
+                <img src="https://media4.giphy.com/media/mA1lQaCF6PLsYBzJAJ/giphy.gif?cid=ecf05e474qehnohkzjjg5260n0iee4u3uve7uzgz6ukyytiy&rid=giphy.gif&ct=g" alt="gif beach" />
+            </figure>
+        </div>
     )
 }
 
