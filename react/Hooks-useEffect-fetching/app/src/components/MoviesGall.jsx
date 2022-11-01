2b2e993d69a2ef10038063a772b5a0ab36ed50da
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-01 13:43:28 +0100

    Hooks-fetchingApi

--- react/Hooks-useEffect-fetching/app/src/components/MoviesGall.jsx
+++ react/Hooks-useEffect-fetching/app/src/components/MoviesGall.jsx
@@ -8,12 +8,7 @@ const MoviesGall = ({ movies }) => {
         {movies.map((movie) => (
             <figure key={movie.id} className="figureGallery">
                 <Link to={`/moviesGallery/${movie.id}`} >
-                    <div>
-                        <h3>{movie.title}</h3>
-                        <h4>{movie.año}</h4>
-                    </div>
                     <img src={movie.poster} alt={movie.title} />
-                    <p>{movie.description}</p>
                 </Link>
             </figure>
         ))}
