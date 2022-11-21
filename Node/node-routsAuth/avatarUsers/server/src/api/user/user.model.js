81f14f0cc0dcb384ef4b439471f097d78f438060
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-21 13:18:17 +0100

    User.model

---
+++ Node/node-routsAuth/avatarUsers/server/src/api/user/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    emogi: { type: String, required: true },
+    avatar: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  //Numero de encriptaciones
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model("users", userSchema);
